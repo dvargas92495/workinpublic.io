012f436946888842faa99d0e45581277a212d585
David Vargas <dvargas92495@gmail.com>
2021-10-31 21:05:30 -0400

    put domain in deploy

--- functions/build-board-page.ts
+++ functions/build-board-page.ts
@@ -5,9 +5,12 @@ import * as data from "../pages/board/[id].data";
 import * as _html from "../pages/_html";
 
 export const handler = ({ uuid }: { uuid: string }) =>
-  isr({ Page, data, _html, params: { id: uuid }, path: "board/[id].js" }).then(() =>
-    process.env.NODE_ENV === "production"
-      ? deploy({}).then(() => console.log("deployed successfully"))
-      : console.log("Wrote locally")
+  isr({ Page, data, _html, params: { id: uuid }, path: "board/[id].js" }).then(
+    () =>
+      process.env.NODE_ENV === "production"
+        ? deploy({ domain: "workinpublic.io" }).then(() =>
+            console.log("deployed successfully")
+          )
+        : console.log("Wrote locally")
   );
 export type Handler = typeof handler;
