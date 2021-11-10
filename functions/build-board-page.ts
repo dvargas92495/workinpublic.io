b982ad9be7159ec2c7f343365836f2d2a01d8523
David Vargas <dvargas92495@gmail.com>
2021-11-10 16:29:33 -0500

    Fix fuego deploy bucket

--- functions/build-board-page.ts
+++ functions/build-board-page.ts
@@ -1,16 +1,12 @@
 import isr from "fuegojs/dist/isr";
-import deploy from "fuegojs/dist/deploy";
+import fuegoDeploy from "./_common/fuegoDeploy";
 import * as Page from "../pages/board/[id]";
 import * as data from "../pages/board/[id].data";
 import * as _html from "../pages/_html";
 
 export const handler = ({ uuid }: { uuid: string }) =>
   isr({ Page, data, _html, params: { id: uuid }, path: "board/[id].js" }).then(
-    () =>
-      process.env.NODE_ENV === "production"
-        ? deploy({ domain: process.env.HOST }).then(() =>
-            console.log("deployed successfully")
-          )
-        : console.log("Wrote locally")
+    fuegoDeploy
   );
+
 export type Handler = typeof handler;
--- functions/build-project-page.ts
+++ functions/build-project-page.ts
@@ -1,16 +1,11 @@
 import isr from "fuegojs/dist/isr";
-import deploy from "fuegojs/dist/deploy";
+import fuegoDeploy from "./_common/fuegoDeploy";
 import * as Page from "../pages/projects/[id]";
 import * as data from "../pages/projects/[id].data";
 import * as _html from "../pages/_html";
 
 export const handler = ({ uuid }: { uuid: string }) =>
   isr({ Page, data, _html, params: { id: uuid }, path: "projects/[id].js" }).then(
-    () =>
-      process.env.NODE_ENV === "production"
-        ? deploy({ domain: process.env.HOST }).then(() =>
-            console.log("deployed successfully")
-          )
-        : console.log("Wrote locally")
+    fuegoDeploy
   );
 export type Handler = typeof handler;
