aeb2f33875e75498bd510621b2cd53fd9a45fef7
David Vargas <dvargas92495@gmail.com>
2021-11-16 20:34:45 -0500

    Include define env object in build embeds script

--- scripts/build-embeds.js
+++ scripts/build-embeds.js
@@ -1,14 +1,29 @@
 const esbuild = require("esbuild").build;
 const fs = require("fs");
 
+const IGNORE_ENV = ["HOME"];
+const getDotEnvObject = () => {
+  const env = {
+    ...Object.fromEntries(
+      Object.entries(process.env)
+        .filter(([k]) => !/[()]/.test(k))
+        .filter(([k]) => !IGNORE_ENV.includes(k))
+    ),
+  };
+  return Object.fromEntries(
+    Object.keys(env).map((k) => [`process.env.${k}`, JSON.stringify(env[k])])
+  );
+};
+
 const build = () => {
-  if (!fs.existsSync("dist")) fs.mkdirSync("dist");
+  if (!fs.existsSync("out")) fs.mkdirSync("out");
   const entryPoints = fs.readdirSync("embeds").map((f) => `embeds/${f}`);
   return esbuild({
     entryPoints,
     bundle: true,
     outdir: "out",
     minify: true,
+    define: getDotEnvObject(),
   })
     .then(() => console.log("Finished!"))
     .catch((e) => {
