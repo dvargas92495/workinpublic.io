30cf78cc81e6e73076a0619801c54758f0efa410
David Vargas <dvargas92495@gmail.com>
2021-11-16 21:59:57 -0500

    safer jsoning

--- embeds/board.tsx
+++ embeds/board.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import { BoardComponent } from "../pages/board/[id]";
 
 const boardId = process.env.FUNDING_BOARD_UUID || "";
-const propString = process.env.FUNDING_BOARD_PROPS || JSON.stringify({ name: "", projects: [] });
+const propString = JSON.stringify(process.env.FUNDING_BOARD_PROPS || { name: "", projects: [] });
 const props = JSON.parse(propString);
 
 const currentScript = document.currentScript;
