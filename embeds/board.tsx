08d16f928308d7f29693018d1132d6c783108a41
David Vargas <dvargas92495@gmail.com>
2021-11-16 22:19:06 -0500

    do everything onload to avoid hydration

--- embeds/board.tsx
+++ embeds/board.tsx
@@ -3,21 +3,26 @@ import ReactDOM from "react-dom";
 import { BoardComponent } from "../pages/board/[id]";
 
 const boardId = process.env.FUNDING_BOARD_UUID || "";
-const propString = JSON.stringify(process.env.FUNDING_BOARD_PROPS || { name: "", projects: [] });
+const propString = JSON.stringify(
+  process.env.FUNDING_BOARD_PROPS || { name: "", projects: [] }
+);
 const props = JSON.parse(propString);
 
 const currentScript = document.currentScript;
-if (document.head.contains(currentScript)) {
-  window.addEventListener("load", () => {
-    ReactDOM.render(<BoardComponent {...props} />, document.getElementById(boardId));
-  });
-} else if (document.body.contains(currentScript)) {
-  const parent = currentScript.parentElement;
-  const container = document.createElement("div");
-  parent.insertBefore(container, currentScript);
-  currentScript.remove();
+window.addEventListener("load", () => {
+  if (document.head.contains(currentScript)) {
+    ReactDOM.render(
+      <BoardComponent {...props} />,
+      document.getElementById(boardId)
+    );
+  } else if (document.body.contains(currentScript)) {
+    const parent = currentScript.parentElement;
+    const container = document.createElement("div");
+    parent.insertBefore(container, currentScript);
+    currentScript.remove();
 
-  ReactDOM.render(<BoardComponent {...props} />, container);
-} else {
-  throw new Error(`Failed to run WorkInPublic embed: Where is it?`);
-}
+    ReactDOM.render(<BoardComponent {...props} />, container);
+  } else {
+    throw new Error(`Failed to run WorkInPublic embed: Where is it?`);
+  }
+});
